7dd7ec7ce4a90c37e549dfa3aba110e0b4c1ebca
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-07-01 22:49:24 +0200

    adding new user to database using payload with type from IUserPayload and find individual user by its Id

--- src/repositories/user.ts
+++ src/repositories/user.ts
@@ -1,3 +1,4 @@
+import exp from 'constants';
 import { getRepository } from 'typeorm';
 import { User } from '../models';
 
@@ -10,4 +11,20 @@ export interface IUserPayload {
 export const getUsers = async (): Promise<Array<User>> => {
     const userRepository = getRepository(User);
     return userRepository.find()
+}
+
+export const createUser = async (payload: IUserPayload): Promise<User> => {
+    const userRepository = getRepository(User);
+    const user = new User();
+    return userRepository.save({
+        ...user,
+        ...payload,
+    })
+};
+
+export const getUser = async (id: number): Promise<User | null> => {
+    const userRepository = getRepository(User);
+    const user = await userRepository.findOne({ id: id });
+    if (!user) return null;
+    return user;    
 }
