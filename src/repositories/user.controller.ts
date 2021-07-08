e6fa9c1ea12cd1ba5052fab735d734ec46804952
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-07-08 18:30:57 +0200

    Controller for the User APIs - UserController Class
    The createUser method is the post request, and the schema of the body is of the IUserPayload type

--- src/repositories/user.controller.ts
+++ src/repositories/user.controller.ts
@@ -1,4 +1,4 @@
-import { Get, Route, Tags, Post, Body, Patch } from 'tsoa';
+import { Get, Route, Tags, Post, Body, Path } from 'tsoa';
 import { User } from '../models';
 import {
     getUsers,
@@ -7,5 +7,23 @@ import {
     getUser
 } from '../repositories/user';
 
+@Route("users")
+@Tags("User")
 
+export default class UserController {
+    @Get("/")
+    public async getUsers(): Promise<Array<User>> {
+        return getUsers()
+    }
+
+    @Post("/")
+    public async createUser(@Body() body: IUserPayload): Promise<User> {
+        return createUser(body)
+    }
+
+    @Get("/:id")
+    public async getUser(@Path() id: string): Promise<User | null> {
+        return getUser(Number(id))
+    }
+}
 
