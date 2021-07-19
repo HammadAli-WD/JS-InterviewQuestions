31247d512e7f2ee92e3985eca6c586a5bb973dc0
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-07-20 01:38:57 +0200

    testing for non empty list

--- src/controllers/user.controller.test.ts
+++ src/controllers/user.controller.test.ts
@@ -14,5 +14,29 @@ describe('UserController', () => {
             expect(spy).toHaveBeenCalledTimes(1);
             spy.mockRestore();
         });
+        test('should return user list', async () => {
+            const usersList = [
+                {
+                    id: 1,
+                    firstName: "firstName",
+                    lastName: "lastName",
+                    email: "email@example.com",
+                    posts: [],
+                    comments: [],
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                }
+            ]
+            const spy = jest
+                .spyOn(UserRepository, "getUsers")
+                .mockResolvedValueOnce(usersList)
+            const controller = new UserController();
+            const users = await controller.getUsers();
+            expect(users).toEqual(usersList);
+            expect(spy).toHaveBeenCalledWith();
+            expect(spy).toHaveBeenCalledTimes(1);
+            spy.mockRestore()
+        })
+        
     });    
 })
