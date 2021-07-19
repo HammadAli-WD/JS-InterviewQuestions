5bccbdc2fb9e40bc85c5276682f12b88c393cbfe
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-07-19 23:13:54 +0200

    Spy of User Repository for get users function was created as a promise of empty array, which is going to be resolved on calling this.
    Through this way, the behavior of the method can be tested by recreating the condition where the list is empty in database. In spying, the original method replaced with a mock function and change its implementation to return the value as per our need.
    After running the test the spy function to the original restored by mockRestore function.

--- src/controllers/user.controller.test.ts
+++ src/controllers/user.controller.test.ts
@@ -3,7 +3,16 @@ import * as UserRepository from "../repositories/user.repository";
 
 describe('UserController', () => {
     describe('getUsers', () => {
-        
-    })
-    
+        test('should return empty array', async () => {
+            const spy = jest
+                .spyOn(UserRepository, "getUsers")
+                .mockResolvedValueOnce([]);
+            const controller = new UserController();
+            const users = await controller.getUsers();
+            expect(users).toEqual([]);
+            expect(spy).toHaveBeenCalledWith();
+            expect(spy).toHaveBeenCalledTimes(1);
+            spy.mockRestore();
+        });
+    });    
 })
