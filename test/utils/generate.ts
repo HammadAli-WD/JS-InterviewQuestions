ddd5f3894f20849f6d6bc9a998a5f846309b6286
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-07-22 21:42:02 +0200

    create dummy data for the test
    The generateUserData function creates the fake user database object and generateUsersData will just return the list of it.

---
+++ test/utils/generate.ts
@@ -0,0 +1,26 @@
+import faker from "faker";
+
+export function generateUserData(overide = {}) {
+  return {
+    id: faker.random.number(),
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    email: faker.internet.email(),
+    posts: [],
+    comments: [],
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    ...overide,
+  };
+}
+
+export function generateUsersData(n: number = 1, overide = {}) {
+  return Array.from(
+    {
+      length: n,
+    },
+    (_, i) => {
+      return generateUserData({ id: i, ...overide });
+    }
+  );
+}
