d8c3cfc7dd606a36942ed4ba4ea4599a9fb7dc55
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-07-24 21:59:01 +0200

    add User and get User testing

--- src/controllers/user.controller.test.ts
+++ src/controllers/user.controller.test.ts
@@ -1,6 +1,10 @@
 import UserController from './user.controller';
 import * as UserRepository from "../repositories/user.repository";
-import {generateUsersData} from '../../test/utils/generate'
+import {
+    generateUsersData,
+    generateUserPayload,
+    generateUserData
+} from '../../test/utils/generate'
 
 afterEach(() => {
     jest.resetAllMocks()
@@ -29,7 +33,50 @@ describe('UserController', () => {
             expect(spy).toHaveBeenCalledWith();
             expect(spy).toHaveBeenCalledTimes(1);
             //spy.mockRestore()
-        })
-        
-    });    
+        })        
+    });
+    
+    describe('addUser', () => {
+        test('should add user to the databse', async () => {
+            const payload = generateUserPayload();
+            const userData = generateUserData(payload);
+            const spy = jest
+                .spyOn(UserRepository, "createUser")
+                .mockResolvedValueOnce(userData);
+            const controller = new UserController()
+            const user = await controller.createUser(payload);
+            expect(user).toMatchObject(payload);
+            expect(user).toEqual(userData);
+            expect(spy).toHaveBeenCalledWith(payload);
+            expect(spy).toHaveBeenCalledTimes(1)
+        });        
+    })
+   describe("getUser ", () => {
+    test("should return user from the database", async () => {
+      const id = 1;
+      const userData = generateUserData({ id });
+      const spy = jest
+        .spyOn(UserRepository, "getUser")
+        .mockResolvedValueOnce(userData);
+      const controller = new UserController();
+      const user = await controller.getUser(id.toString());
+      expect(user).toEqual(userData);
+      expect(user?.id).toBe(id);
+      expect(spy).toHaveBeenCalledWith(id);
+      expect(spy).toHaveBeenCalledTimes(1);
+    });
+
+    test("should return null if user not found", async () => {
+      const id = 1;
+      const spy = jest
+        .spyOn(UserRepository, "getUser")
+        .mockResolvedValueOnce(null);
+      const controller = new UserController();
+      const user = await controller.getUser(id.toString());
+      expect(user).toBeNull();
+      expect(spy).toHaveBeenCalledWith(id);
+      expect(spy).toHaveBeenCalledTimes(1);
+    });
+  }); 
+    
 })
